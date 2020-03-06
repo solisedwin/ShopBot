56687ba9b3fbd11b7d280ef509fc5b836ecc25c1
solisedwin <solisedwin10@gmail.com>
2020-03-05 23:05:54 -0500

    Fixed bug of not clicking items, which in turn made us not be able to go to the checkout page. Also, if checkout page doesnt load, we run bot again. Handle specifc error if element is not found in clothing article page, then perhaps it has been removed. Cleaner design layout as we added the dropdown checkout values for purchasing order.

--- src/supreme.py
+++ src/supreme.py
@@ -20,7 +20,7 @@ from urllib.request import Request, urlopen
 
 import time
 import schedule
-import random
+import random	
 import requests
 import json
 import sys
@@ -60,18 +60,21 @@ class SupremeWeb(object):
 			print('Supreme site isnt open now') 
 
 
-
 	def run_bot(self):
 
 		#schedule.every(4).seconds.do(self.access_home_site).tag('kicked-out')   
 		schedule.every(3.5).seconds.do(self.is_kicked_out)
 		# self.run_schedule()
-		self.json_clothing_orders()
-		#self.driver.close()        
+		self.read_json_clothing_orders()
+		
+		"""
+		Close entire application
+		self.driver.close()       
+		sys.exit(1)
+		"""
 
 
-
-	def json_clothing_orders(self):
+	def read_json_clothing_orders(self):
 
 		with open('customer.json') as file:
 			json_file = json.load(file)
@@ -80,7 +83,6 @@ class SupremeWeb(object):
 			for clothing_article in clothes:
 			
 				clothing_article = clothing_article.strip()
-				
 				self.load_clothing_page(clothing_article)
 
 				for info in clothes[clothing_article]:
@@ -90,32 +92,30 @@ class SupremeWeb(object):
 					size = info['size'].strip()
 
 					#Tuple data structure to perserve/pack clothing information together
-					clothing_info = (name, color, size)				
-					self.search_match_clothes(clothing_info)
-				
-					
+					clothing_info = (name, color, size)             
+					self.search_match_clothes(clothing_info)			
 
-			self.checkout_items()
-				
-			  
+			file.close()
+
+
+			self.read_json_paymentinfo()    
+					  
 
 
 	def is_kicked_out(self):
 		current_url = self.driver.current_url
 
 		#We are kicked out of page. Website error due to traffic
-		if 'out_of_stock' in current_url or '/shop/all' not in current_url:
+		if 'out_of_stock' in current_url:
 			schedule.every(3).seconds.do(self.access_home_site).tag('kicked-out')   
 		else:
 			print('Not kicked out yet')
-
 
 
 	def run_schedule(self):
 		while True:
 			schedule.run_pending()
 			time.sleep(1)
-
 
 			
 	def load_clothing_page(self, clothing_article):
@@ -127,8 +127,7 @@ class SupremeWeb(object):
 		wait.until(EC.presence_of_element_located((By.ID, "container")))
 	
 
-
-	def search_match_clothes(self, clothing_info):	
+	def search_match_clothes(self, clothing_info):  
 
 		clothing_name = clothing_info[0]
 		clothing_color = clothing_info[1]
@@ -136,9 +135,7 @@ class SupremeWeb(object):
 
 		item_xpath_locator = '//div[@class="inner-article" and .//*[contains(text(), "{}")] and .//*[contains(text(), "{}")]]'.format(clothing_name, clothing_color)
 			
-
 		print("Looking at " + str(clothing_name) + " with color " + str(clothing_color) + " at size " + str(clothing_size))
-
 
 		try:
 			
@@ -157,13 +154,17 @@ class SupremeWeb(object):
 
 			if 'sold out' in tag_text:
 				print("~~ Item is sold out ")
-			else:				
+			else:               
 				
 				self.add_to_cart(sold_out_tag)
+				"""
 				self.driver.back()
 				self.driver.refresh();
+				"""	
+		except NoSuchElementException as no_element:
+			print('~~ Element cant be found. Perhaps has been removed from website')
 		except Exception as e:
-			print('~~ 	Error: ' + str(e))
+			print('~~   Error: ' + str(e))
 
 
 
@@ -175,71 +176,91 @@ class SupremeWeb(object):
 		wait.until(EC.element_to_be_clickable((By.ID, "details")))      
 		
 		add_to_cart_btn = self.driver.find_element_by_css_selector('#add-remove-buttons > input');              
-	
-		action = ActionChains(self.driver)
-		hov = action.move_to_element(add_to_cart_btn).click().perform
 		
-		wait.until(EC.presence_of_element_located((By.ID, "cart")))
+		action = ActionChains(self.driver)
+		hov = action.move_to_element(add_to_cart_btn).click().perform()
+
+		#wait.until(EC.presence_of_element_located((By.ID, "cart")))
+		wait.until(EC.presence_of_element_located((By.CLASS_NAME, "in-cart")))
+		
 		print('** Added item to cart ** ')
 		print()
 
-		
 
+		
 	def click_item(self, item):
 		action = ActionChains(self.driver)
 		action.click(item).perform()
 
 
-	def process_payment(self, action):
-		payment_btn = self.driver.find_element_by_css_selector('input.button')
-		action.move_to_element(payment_btn).click().perform() 
+
+	def read_json_paymentinfo(self):
+		with open('customer.json') as file:
+
+			json_file = json.load(file) 	
+
+			customer_info = json_file['payment']
+
+			name = customer_info['name']
+			email = customer_info['email']
+			phone = customer_info['phone']
+			address = customer_info['address']
+			apt_num = customer_info['apt_num']
+			zip_code = customer_info['zip']
+			town_name = customer_info['town']
+			card_num  = customer_info['card']
+			cvv_num = customer_info['cvv']
+
+			payment_information = (name, email, phone, address, apt_num,  zip_code, town_name , card_num, cvv_num)
+
+			state = customer_info["state"]
+			country = customer_info["country"]
+			card_expiration_month = customer_info["card_expiration_month"]
+			card_expiration_year = customer_info["card_expiration_year"]
+
+			dropdown_payment_info = (state, country, card_expiration_month, card_expiration_year)
+
+			self.checkout_items(payment_information, dropdown_payment_info)
+			file.close()
 
 
-	def checkout_items(self):
+	def checkout_items(self, payment_information, dropdown_payment_info):
 
 		#Redirect to process page
-		self.driver.get('https://www.supremenewyork.com/checkout')
+		self.driver.get("https://www.supremenewyork.com/checkout")
 
-		# Inputing for all input tags
-		print('Item(s) has been checked out. We are PAYING for it now')
+		try:
+			wait = WebDriverWait(self.driver,4)
+			wait.until(EC.presence_of_element_located((By.ID, "cart-body")))
+			#All input tags
+			input_tags = self.driver.find_elements_by_css_selector('input.string')
 
-		input_data = ['fake name', 'fake_email10@aol.com', '1111111111', '78 cat street', '3L', '12345', 'Oniondale', '5412 7543 5678', '503']
+			for index,tag in enumerate(input_tags):
+				tag.send_keys(payment_information[index])
 
-		wait = WebDriverWait(self.driver, 4)
-		wait.until(EC.presence_of_element_located((By.ID, "cart-body")))
-
-
-		#All input tags
-		input_tags = self.driver.find_elements_by_css_selector('input.string')
-	
-		
-		for index,tag in enumerate(input_tags):
-			tag.send_keys(input_data[index])
-
-		
-		select_data = ['NY','USA', '10', '2022']
-
-		#Select choices for select tags 
-		select_tags = self.driver.find_elements_by_css_selector('select')
-		action = ActionChains(self.driver)
+			#Select choices for select tags 
+			select_tags = self.driver.find_elements_by_css_selector('select')
+			action = ActionChains(self.driver)
 
 
-		for index, tag in enumerate(select_tags):
+			for index, tag in enumerate(select_tags):
 
-			element = WebDriverWait(self.driver, 4).until(EC.element_to_be_clickable((By.ID, tag.get_attribute("id"))))        
-			action.move_to_element(element)
+				element = WebDriverWait(self.driver, 4).until(EC.element_to_be_clickable((By.ID, tag.get_attribute("id"))))        
+				action.move_to_element(element)
+					
+				select = Select(element)
+				select.select_by_value(dropdown_payment_info[index])
+
+			action.click(self.driver.find_elements_by_css_selector('.iCheck-helper')[1]);
 			
-			select = Select(element)
-			select.select_by_value(select_data[index])
+			self.click_payment_button(action)
+
+		except TimeoutException as e:
+			print("@@ Too much time has passed to checkout/load payment page")
+			self.run_bot()
+	
 
 
-		action.click(self.driver.find_elements_by_css_selector('.iCheck-helper')[1]);
-		
-		self.driver.implicitly_wait(1)
-		self.process_payment(action)
-
-
-
-
-
-
+	def click_payment_button(self, action):
+		payment_btn = self.driver.find_element_by_css_selector('input.button')
+		action.move_to_element(payment_btn).click().perform() 
